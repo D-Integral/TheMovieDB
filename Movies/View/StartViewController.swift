2839046b4fa87f809e674ee95c2a39456d261003
Dmytro Skorokhod <dima.skorokhod@gmail.com>
2019-07-07 19:13:03 +0300

    The start animation.

--- Movies/View/StartViewController.swift
+++ Movies/View/StartViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class LaunchViewController: UIViewController {
+class StartViewController: UIViewController {
     
     @IBOutlet weak var square: UIView!
+    
     var horizontalConstraint: NSLayoutConstraint? = nil
     var verticalConstraint: NSLayoutConstraint? = nil
+    
+    var widthConstraint: NSLayoutConstraint? = nil
+    var heightConstraint: NSLayoutConstraint? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,12 +33,98 @@ class LaunchViewController: UIViewController {
         let theVerticalConstraint = NSLayoutConstraint(item: square!, attribute: NSLayoutConstraint.Attribute.centerY, relatedBy: NSLayoutConstraint.Relation.equal, toItem: view, attribute: NSLayoutConstraint.Attribute.centerY, multiplier: 1, constant: 0)
         verticalConstraint = theVerticalConstraint
         
-        NSLayoutConstraint.activate([theHorizontalConstraint, theVerticalConstraint])
+        let theWidthConstraint = NSLayoutConstraint(item: square!, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        widthConstraint = theWidthConstraint
         
+        let theHeightConstraint = NSLayoutConstraint(item: square!, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        heightConstraint = theHeightConstraint
+        
+        NSLayoutConstraint.activate([theHorizontalConstraint, theVerticalConstraint, theWidthConstraint, theHeightConstraint])
+        
+        addMoveToTheLeft()
+    }
+    
+    func addMoveToTheLeft() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
             guard let this = self else { return }
             
-            this.horizontalConstraint?.constant -= this.view.bounds.width / 4
+            UIView.animate(withDuration: 0.2, animations: {
+                if let theOldHorizontalConstraint = this.horizontalConstraint {
+                    NSLayoutConstraint.deactivate([theOldHorizontalConstraint])
+                }
+                
+                let theNewHorizontalConstraint = NSLayoutConstraint(item: this.square!, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: this.view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: -(this.view.bounds.width / 4))
+                this.horizontalConstraint = theNewHorizontalConstraint
+                NSLayoutConstraint.activate([theNewHorizontalConstraint])
+                this.view.layoutIfNeeded()
+            })
+            
+            this.addMoveToTheRight()
+        }
+    }
+    
+    func addMoveToTheRight() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let this = self else { return }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                if let theOldHorizontalConstraint = this.horizontalConstraint {
+                    NSLayoutConstraint.deactivate([theOldHorizontalConstraint])
+                }
+                
+                let theNewHorizontalConstraint = NSLayoutConstraint(item: this.square!, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: this.view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: (this.view.bounds.width / 4))
+                this.horizontalConstraint = theNewHorizontalConstraint
+                NSLayoutConstraint.activate([theNewHorizontalConstraint])
+                this.view.layoutIfNeeded()
+            })
+            
+            this.addMoveToTheCenter()
+        }
+    }
+    
+    func addMoveToTheCenter() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let this = self else { return }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                if let theOldHorizontalConstraint = this.horizontalConstraint {
+                    NSLayoutConstraint.deactivate([theOldHorizontalConstraint])
+                }
+                
+                let theNewHorizontalConstraint = NSLayoutConstraint(item: this.square!, attribute: NSLayoutConstraint.Attribute.centerX, relatedBy: NSLayoutConstraint.Relation.equal, toItem: this.view, attribute: NSLayoutConstraint.Attribute.centerX, multiplier: 1, constant: 0)
+                this.horizontalConstraint = theNewHorizontalConstraint
+                NSLayoutConstraint.activate([theNewHorizontalConstraint])
+                this.view.layoutIfNeeded()
+            })
+            
+            this.addSplitGroupAnimation1()
+        }
+    }
+    
+    func addSplitGroupAnimation1() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let this = self else { return }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                this.square.layer.cornerRadius = this.square.bounds.width / 2;
+                this.square.layer.masksToBounds = true;
+                this.view.layoutIfNeeded()
+            })
+            
+            this.addColorChange()
+        }
+    }
+    
+    func addColorChange() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) { [weak self] in
+            guard let this = self else { return }
+            
+            UIView.animate(withDuration: 0.2, animations: {
+                this.square.backgroundColor = UIColor.blue
+                this.view.layoutIfNeeded()
+            })
+            
+            this.addSplitGroupAnimation1()
         }
     }
     
